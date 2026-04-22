da89109ec6308d03dcc7069cd5de77d1da9f19bc
shinchaerin79 <shinchaerin79@gmail.com>
2026-04-22 16:34:30 +0900

    :sparkles: Feat: 콘텐츠 관리 페이지 라우팅 추가

--- src/app/routes/Router.jsx
+++ src/app/routes/Router.jsx
@@ -3,6 +3,7 @@ import AdminLayout from '../layouts/AdminLayout';
 import LoginPage from '../../features/auth/pages/LoginPage';
 import MemberListPage from '../../features/member/pages/MemberListPage';
 import MemberDetailPage from '../../features/member/pages/MemberDetailPage';
+import ContentManagementPage from '../../features/content/pages/ContentManagementPage';
 import { PATH } from './path';
 
 export default function Router() {
@@ -14,6 +15,7 @@ export default function Router() {
         <Route element={<AdminLayout />}>
           <Route path={PATH.MEMBER_LIST} element={<MemberListPage />} />
           <Route path={PATH.MEMBER_DETAIL} element={<MemberDetailPage />} />
+          <Route path={PATH.CONTENT} element={<ContentManagementPage />} />
         </Route>
 
         <Route path="*" element={<Navigate to={PATH.MEMBER_LIST} replace />} />
